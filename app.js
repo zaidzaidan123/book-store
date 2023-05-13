9b9447f1fb50f11ccb1b6dc8b9938b2bee0502e3
zaidzaidan <zaidzaidan79@gmail.com>
2023-05-13 22:51:28 +0300

    finish the dark and the fav parts

---
+++ app.js
@@ -0,0 +1,14 @@
+let btn = document.getElementById("btn")
+let fav=document.getElementById("fav-button")
+btn.onclick = ()=>{
+    document.body.classList.toggle("dark-theme")
+}
+document.getElementById("fav").style.display="none"
+fav.onclick=()=>{
+    if(document.getElementById("fav").style.display==="none"){
+        document.getElementById("fav").style.display="block"
+    }
+    else{
+        document.getElementById("fav").style.display="none"
+    }
+}
